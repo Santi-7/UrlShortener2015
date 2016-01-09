5cff262b2edcceaa5cc590c42a77d997c46f957d
Your real Here <alberto.alvarez.aldea@hotmail.com>
2016-01-09 20:06:48 +0100

    Fail trying to solve

--- candy-pink/src/main/java/urlshortener2015/candypink/ApplicationConfig.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/ApplicationConfig.java
@@ -1,8 +1,14 @@
 package urlshortener2015.candypink;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import urlshortener2015.candypink.auth.JWTokenFilter;
+import urlshortener2015.candypink.auth.support.AuthURI;
+import urlshortener2015.candypink.auth.support.AuthUtils;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by david on 2/01/16.
@@ -12,4 +18,14 @@ import org.springframework.context.annotation.Import;
 @Import(checker.WebServiceConfig.class)
 public class ApplicationConfig {
 
+	@Value("${jwt.key}")
+	private String key;
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		JWTokenFilter authenticationFilter = new JWTokenFilter(key, AuthUtils.buildAuthURIs());
+		registrationBean.setFilter(authenticationFilter);
+		return registrationBean;
+	}
 }
