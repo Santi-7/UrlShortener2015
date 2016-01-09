7229d2bea44bf0d67bcdc3718523fd1d737237b9
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-09 11:14:02 +0100

    Imports added

--- candy-pink/src/main/java/urlshortener2015/candypink/web/ExceptionHandlingController.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/web/ExceptionHandlingController.java
@@ -2,7 +2,11 @@ package urlshortener2015.candypink.web;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import urlshortener2015.candypink.exceptions.ResourceNotFoundException;
 
 @Controller
 public class ExceptionHandlingController {
