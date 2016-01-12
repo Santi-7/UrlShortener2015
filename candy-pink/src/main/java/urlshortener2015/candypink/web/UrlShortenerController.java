c296e771449ed3d3751c4106e899d7efd1383812
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-12 13:02:10 +0100

    Fixed Admin -> ManageUsers

--- candy-pink/src/main/java/urlshortener2015/candypink/web/UrlShortenerController.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/web/UrlShortenerController.java
@@ -65,7 +65,7 @@ public class UrlShortenerController {
      * @param id    - hash of the shortUrl
      * @param token - optional, token of the shorturl if it is safe
      */
-    @RequestMapping(value = "/{id:(?!link|index|login|signUp|profile|admin|incorrectToken|uploader|errorSpam|noMore|403|fishyURL).*}",
+    @RequestMapping(value = "/{id:(?!link|index|login|signUp|profile|manageUsers|incorrectToken|uploader|errorSpam|noMore|403|fishyURL).*}",
             method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON})
     public ResponseEntity<?> redirectToJSON(@PathVariable String id,
                                             @RequestParam(value = "token", required = false) String token,
