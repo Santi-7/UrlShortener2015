f1e8b1b57e90cb8c91f63ade773eecfd713f2a61
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-12 15:48:45 +0100

    Doc to SignUpController

--- candy-pink/src/main/java/urlshortener2015/candypink/web/SignUpController.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/web/SignUpController.java
@@ -17,23 +17,33 @@ import urlshortener2015.candypink.repository.UserRepositoryImpl;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * This class manages a sign up of a user
+ * It manages a get request to redirect a user to the sign up page
+ * and a post request to sign up a user who has introduced his details.
+ * @author - A.Alvarez, I.Gascon, S.Gil, D.Nicuesa
+ */
 @RestController
 @RequestMapping("/signUp")
 public class SignUpController {
 
+	// Logger
 	private static final Logger logger = LoggerFactory.getLogger(SignUpController.class);
-	
+
+	// Database
 	@Autowired
 	protected UserRepository userRepository;
 
+	/** Default constructor of the controller */
 	public SignUpController() {}
 
+	/** Constructor with database "shortURLRepository" */
 	public SignUpController(UserRepositoryImpl userRepository){
         	this.userRepository = userRepository;
     	}
 
 	/**
-	 *
+	 * It redirects the user to the signUp page
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView register() {
@@ -42,8 +52,18 @@ public class SignUpController {
 		model.setViewName("signUpPage");
 		return model;
 	}
+	
 	/**
-	 *
+	 * It creates a user with the details passed by parameter and returns
+	 * this user in JSON format and a CREATED http code.
+	 * If any of the user information is null, it returns Bad Request code
+	 * If there is already a user with same username or email,
+	 * it returns Conflict code.
+	 * @param username - username of the new user
+	 * @param password - password of the new user
+	 * @param email - email of the new user
+	 * @param authority - authority (role) of the new user
+	 * @param request - request of the client
 	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<User> register(@RequestParam("username") String username,
@@ -51,30 +71,30 @@ public class SignUpController {
 			        @RequestParam("authority") String authority, HttpServletRequest request) {
 		logger.info("Requested registration with username " + username + password + email + transform(authority));
 		User user = new User(username, password, true, email, transform(authority));
-		//Verify the fields aren´t empty
-		if(verifyFields(user)) {
-		  logger.info("Requested registration verified");
-		  // There are users with the same username
-		  if(userRepository.findByUsernameOrEmail(username) != null) {
-		    return new ResponseEntity<>(HttpStatus.CONFLICT);
-		  }
-		  // There are users with the same email
-		  else if(userRepository.findByUsernameOrEmail(email) != null) {
-		    return new ResponseEntity<>(HttpStatus.CONFLICT);
-		  }
-		  // There aren´t other users with this username or email
-		  else {
-		    BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
-		    user.setPassword(encoder.encode(password));
-		    logger.info("Requested registration correct");
-		    user = userRepository.save(user);
-		    logger.info("Requested registration done of user " + user.getUsername());
-         	    return new ResponseEntity<>(user, HttpStatus.CREATED);
-		  }
+		// Verify the fields aren´t empty
+		if (verifyFields(user)) {
+		  	logger.info("Requested registration verified");
+		  	// There are users with the same username
+		  	if(userRepository.findByUsernameOrEmail(username) != null) {
+		    		return new ResponseEntity<>(HttpStatus.CONFLICT);
+		  	}
+			 // There are users with the same email
+		  	else if(userRepository.findByUsernameOrEmail(email) != null) {
+		    		return new ResponseEntity<>(HttpStatus.CONFLICT);
+		  	}
+		  	// There aren´t other users with this username or email
+		  	else {
+		    		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+		    		user.setPassword(encoder.encode(password));
+		    		logger.info("Requested registration correct");
+		    		user = userRepository.save(user);
+		    		logger.info("Requested registration done of user " + user.getUsername());
+         			return new ResponseEntity<>(user, HttpStatus.CREATED);
+			}
 		}
 		// There are empty fields
 		else {
-		  return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 	}
 
@@ -107,13 +127,20 @@ public class SignUpController {
 	  	return true;
 	}
 	
+	/**
+	 * Converts the role "role" to the standar role managed by the application
+	 * @param role - role to be standared
+	 */
 	private String transform(String role) {
+		// Role is normal
 		if (role.equals("Normal")) {
 			return "ROLE_NORMAL";
 		}
+		// Role is premium
 		else if (role.equals("Premium")) {
 			return "ROLE_PREMIUM";
 		}
+		// It is an unknown role
 		else {
 			return null;
 		}
