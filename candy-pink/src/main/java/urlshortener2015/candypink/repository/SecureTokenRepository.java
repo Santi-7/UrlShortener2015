c26603fab57e05c236c2dda9fcae91aa25221f2d
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-12 06:52:19 +0100

    securetoken

--- candy-pink/src/main/java/urlshortener2015/candypink/repository/SecureTokenRepository.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/repository/SecureTokenRepository.java
@@ -6,9 +6,20 @@ import urlshortener2015.candypink.domain.SecureToken;
 
 public interface SecureTokenRepository {
 
+	/**
+	 * Search the secure token in the database. Return the token if it exist
+	 * and null in other case.
+	 * @param token - Token to search
+	 * @return the token if it exist
+	 * and null in other case.
+	 */
 	SecureToken findByToken(String token);
 
+	/**
+	 * Save the secure token in the database. Return the 
+	 * @param token - Token to save
+	 */
 	SecureToken save(SecureToken token);
 
-	void delete(String token);
+	boolean delete(String token);
 }
