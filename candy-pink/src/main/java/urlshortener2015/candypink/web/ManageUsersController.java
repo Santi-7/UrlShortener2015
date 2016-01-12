9a43a71d0ddc0e1e3a98fbf1283b18230778ce6d
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-12 11:29:57 +0100

    Doc to ManageUsersController

--- candy-pink/src/main/java/urlshortener2015/candypink/web/ManageUsersController.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/web/ManageUsersController.java
@@ -11,21 +11,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import urlshortener2015.candypink.repository.UserRepository;
 import urlshortener2015.candypink.repository.UserRepositoryImpl;
 
+/**
+ * This class manages the users of the database.
+ * It can only be accessed with administrator permisions
+ * It manages a get request to return all users of the database
+ * and a post request to delete the desired user.
+ * @author - A.Alvarez, I.Gascon, S.Gil, D.Nicuesa
+ */
 @RestController
 @RequestMapping("/manageUsers")
 public class ManageUsersController {
 
+	// Logger
 	private static final Logger logger = LoggerFactory.getLogger(ManageUsersController.class);
 	
+	// Database
 	@Autowired
 	protected UserRepository userRepository;
 
+	/** Default constructor of the controller */
 	public ManageUsersController() {}
 
+	/** Constructor with database "userRepository" */
 	public ManageUsersController(UserRepositoryImpl userRepository) {
 		this.userRepository = userRepository;
   	}
   
+  	/**
+  	 * It returns all users of the database
+  	 */
   	@RequestMapping(method = RequestMethod.GET)
   	public ModelAndView getUsers() {
 		logger.info("Requested all users info");
@@ -37,6 +51,10 @@ public class ManageUsersController {
 		return model;
   	}
 
+	/**
+	 * It deletes the user with username "user"
+	 * and returns all the remaining users
+	 */
 	@RequestMapping(method = RequestMethod.POST)
   	public ModelAndView deleteUser(@RequestParam String user) {
 		logger.info("Requested delete of username " + user);
